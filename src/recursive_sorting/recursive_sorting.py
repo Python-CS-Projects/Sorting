dab82650875ef28699cc1ff1528564667309942a
fritzgt <tuanis@live.com>
2020-02-05 04:41:02 +0200

    new logic for recursive

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -3,26 +3,24 @@ def merge(arrA, arrB):
     elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
     # TO-DO
-    arr = arrA + arrB
-    for i in range(0, len(arr)-1):
-        cur_index = i  # [*1*, 5, 8, 4] = 0 index
-        # save smallest value
-        smallest_index = cur_index  # 0 index
-
-        for j in range(cur_index, len(arr)):
-            # compare the current smallest value to the rest of array
-            # if there is a smaller item then set the smallest_index = that item
-            if arr[smallest_index] > arr[j]:  # [1, *5*, 8, *4* ]
-                # if there is a smaller item than the current save it
-                smallest_index = j  # index 1 now-> index 3
-
-        # swap bigger values to the right and smaller to left
-        temp = arr[smallest_index]  # [1, 5, 8, *4*]
-        # smaller value = current value (we move the bigger to the right)
-        arr[smallest_index] = arr[cur_index]  # [1, *5, 8, *4] => [1, 5, 8, 5]
-        # Set current value to equal the smaller (left = smaller)
-        arr[cur_index] = temp  # [1, 5, 8, 5] => [1, 4, 8, 5]
-
+    index = 0
+    for i in range(merge_arr)
+     # 3.LHS is empty
+     if len(arrA) == 1:
+        # copy RHS to the merged_arr
+        merged_arr[i] = arrA[0]
+      # 4.RHS is empty
+      elif len(arrB) == 1:
+        # copy LHS to the merged_arr
+         merged_arr[i] = arrB[0]
+    # 1.front of LHS < front of RHS
+      elif arrA[0] < arrB[0]:
+        merged_arr[i] = arrA[0]
+        arrA.pop(0)
+    # 2.front of RHS < front of LHS
+      elif arrA[0] < arrB[0]:
+        merged_arr[i] = arrB[0]
+        arrB.pop(0)
     return arr
 
 
@@ -32,21 +30,23 @@ def merge_sort(arr):
     arr1 = []
     arr2 = []
     # if the array is still bigger than 1 split
+
     if len(arr) > 1:
+      return arr
+    else:
         # middle of the array rounded
-        middle = int(len(arr) / 2)
+        middle = len(arr) // 2
         # first half
         arr1 = arr[:middle]
         # second half
         arr2 = arr[middle:]
-
         # recursive
         merge_sort(arr1)
         merge_sort(arr2)
+        # Start merging by invoking the above method
+        merge(arr1,arr2)
+ 
 
-        # Start merging
-
-    return arr
 
 
 # # STRETCH: implement an in-place merge sort algorithm
